bf982a5b517533e3fbc896aaefb3986abda88e99
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2025-04-13 16:43:11 -0400

    V full before stopping printer

--- prtdaemon/stop.c
+++ prtdaemon/stop.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 
 int main() {
 	FILE* fp;
 	fp = fopen("sync_info.txt", "r");
 	
-	// Seek to stop id formatted as follows:
+	// sync_info.txt is expected to be formatted as follows:
 	// buffer_size (int) \n (char)
 	// buffer_id (int) \n (char)
-	// stop_id (int) ...
+	// stop_id (int) \n (char)
+	// empty_id (int) \n (char)
+	// full_id (int) \n (char)
+	// mutex (int) \n (char)
 	fseek(fp, (sizeof(int) + sizeof(char))* 2, SEEK_SET);
 	
 	int stop_id;
 	fscanf(fp, "%d", &stop_id);
+
+	// Rewind fp to point at the beginning of the file
+	rewind(fp);
+	fseek(fp, (sizeof(int) + sizeof(char))* 5, SEEK_SET);
+	int full_id;
+	fscanf(fp, "%d", &full_id);
+
+	// Wake printer up in case it's blocked
+	v(0, full_id);
 
 	int* stop = (int* ) shmat(stop_id, NULL, SHM_RND);
 
@@ -22,3 +35,11 @@ int main() {
 	return 0;
 }
 
+v(int s, int sem_id) {
+        struct sembuf sops;
+
+        sops.sem_num = s;
+        sops.sem_op = 1;
+        sops.sem_flg = 0;
+        if((semop(sem_id, &sops, 1)) == -1) printf("%s","'V' error\n");
+}
