44c5faeb4fea16341b29990c7cf457a669e50869
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2025-04-13 15:11:54 -0400

    Initialize and clean up shared resources

--- prtdaemon/daemon.c
+++ prtdaemon/daemon.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
 
 struct Job {
 	int owner_id;
@@ -18,6 +20,26 @@ int main(int argc, char* argv[]) {
 		printf("Error: Please enter a buffer size from 1-10 as a cmdline argument\n");
 		return -1;
 	}
+
+	// Get shared memory buffer
+	int shm_id = shmget(IPC_PRIVATE, sizeof(struct Job) * buffer_size, 0700);
+
+	// Get semaphores
+	int empty_id = semget(IPC_PRIVATE, 1, 0700);
+	int full_id = semget(IPC_PRIVATE, 1, 0700);
+	int mutex = semget(IPC_PRIVATE, 1, 0700);
+
+	// Initialize semaphores
+	semctl(empty_id, 0, SETVAL, buffer_size);
+	semctl(full_id, 0, SETVAL, 0);
+	semctl(mutex, 0, SETVAL, 1);
+
+
+	// Clean up
+	shmctl(shm_id, IPC_RMID, NULL);
+	semctl(empty_id, 0, IPC_RMID, 0);
+	semctl(full_id, 0, IPC_RMID, 0);
+	semctl(mutex, 0, IPC_RMID, 0);
 
 	return 0;
 }
