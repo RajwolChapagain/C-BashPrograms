5a20ca6879c0c56d85ec9cba203dc8512a1be5cc
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2025-04-24 14:04:29 -0400

    Copy prtdaemon files and modify name and comments

---
+++ mm1/consumer.c
@@ -0,0 +1,112 @@
+// Name:		Rajwol Chapagain
+// Class:		CSC 460
+// Assignment:	Memory Manager - A program that simulates a memory management system that takes in requests from processes wanting to be put into RAM, 
+// 				places them using the first fit algorithm for contiguous memory allocation, and simulates executing them.
+// Date: 		04/24/2025
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+struct Job {
+	int owner_id;
+	char filename[20];
+};
+
+int main(int argc, char* argv[]) {
+	FILE* fp;
+	fp = fopen("sync_info.txt", "r");
+
+	if (fp) {
+		printf("Error: sync_info.txt already exists in the current directory. Is another print daemon running?\n");
+		fclose(fp);
+		return 1;
+	}
+
+	if (argc < 2) {
+		printf("Error: Please enter a buffer size from 1-10 as a cmdline argument\n");
+		return 1;
+	}
+
+	int buffer_size = atoi(argv[1]);
+
+	if (buffer_size < 1 || buffer_size > 10) {
+		printf("Error: Please enter a buffer size from 1-10 as a cmdline argument\n");
+		return 1;
+	}
+
+	// Get shared memory
+	int shm_id = shmget(IPC_PRIVATE, sizeof(struct Job) * buffer_size, 0700);
+	int stop_id = shmget(IPC_PRIVATE, sizeof(int), 0700);
+	int rear_id = shmget(IPC_PRIVATE, sizeof(int), 0700);
+
+	// Get semaphores
+	int empty_id = semget(IPC_PRIVATE, 1, 0700);
+	int full_id = semget(IPC_PRIVATE, 1, 0700);
+	int mutex = semget(IPC_PRIVATE, 1, 0700);
+
+	// Initialize buffer front and rear
+	int front = 0;
+	int* rear_addr = (int*) shmat(rear_id, NULL, SHM_RND);
+	*rear_addr = 0;	
+
+	// Initialize semaphores
+	semctl(empty_id, 0, SETVAL, buffer_size);
+	semctl(full_id, 0, SETVAL, 0);
+	semctl(mutex, 0, SETVAL, 1);
+
+	// Save info to file
+	fp = fopen("sync_info.txt", "w");
+	fprintf(fp, "%d\n%d\n%d\n%d\n%d\n%d\n%d\n", buffer_size, shm_id, stop_id, empty_id, full_id, mutex, rear_id);
+	fclose(fp);
+
+	int *stop = (int *) shmat(stop_id, NULL, SHM_RND);
+	*stop = 0;
+
+	struct Job* buffer_addr = (struct Job*) shmat(shm_id, NULL, SHM_RND);
+	// Print loop
+	while (!*stop) {
+		p(0, full_id);
+		if (*stop) break; // stop.c could've woken it up
+		p(0, mutex);
+		printf("Printing %d's file...\n", buffer_addr[front].owner_id);
+		char command[512];
+		sprintf(command, "cat %s", buffer_addr[front].filename);
+		system(command);
+		front = (front + 1) % buffer_size;
+		v(0, mutex);
+		v(0, empty_id);
+	}
+
+	// Clean up
+	shmctl(shm_id, IPC_RMID, NULL);
+	shmctl(stop_id, IPC_RMID, NULL);
+	shmctl(rear_id, IPC_RMID, NULL);
+	semctl(empty_id, 0, IPC_RMID, 0);
+	semctl(full_id, 0, IPC_RMID, 0);
+	semctl(mutex, 0, IPC_RMID, 0);
+	remove("sync_info.txt");
+	system("rm quotefile* &> /dev/null");
+
+	printf("Printer shutting down.\n");
+	return 0;
+}
+
+p(int s,int sem_id) {
+        struct sembuf sops;
+
+        sops.sem_num = s;
+        sops.sem_op = -1;
+        sops.sem_flg = 0;
+        if((semop(sem_id, &sops, 1)) == -1) printf("%s", "'P' error\n");
+}
+
+v(int s, int sem_id) {
+        struct sembuf sops;
+
+        sops.sem_num = s;
+        sops.sem_op = 1;
+        sops.sem_flg = 0;
+        if((semop(sem_id, &sops, 1)) == -1) printf("%s","'V' error\n");
+}
---
+++ mm1/producer.c
@@ -0,0 +1,95 @@
+// Name:		Rajwol Chapagain
+// Class:		CSC 460
+// Assignment:	Memory Manager - A program that simulates a memory management system that takes in requests from processes wanting to be put into RAM, 
+// 				places them using the first fit algorithm for contiguous memory allocation, and simulates executing them.
+// Date: 		04/24/2025
+// Producer code that simulates processes requesting to be put into RAM
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+struct Job {
+	int owner_id;
+	char filename[20];
+};
+
+int main() {
+	// Read shared resource info from file created by daemon
+	FILE* fp;
+	fp = fopen("sync_info.txt", "r");
+
+	if (!fp) {
+		printf("Error: Could not find sync_info.txt in the current directory. Did you forget to run the print daemon first?\n");
+		return 1;
+	}
+
+	int pid = getpid();
+	char command[512];
+
+	// Download and save unique quote in a unique file
+	sprintf(command, "curl -s http://api.quotable.io/random | cut -d ':' -f 3 | cut -d '\"' -f 2 > quotefile_%d", pid);
+	system(command);
+
+	// Create a struct containing file metadata
+	struct Job print_job;
+	print_job.owner_id = pid;
+	sprintf(print_job.filename, "quotefile_%d", pid);
+
+	int buffer_size, buffer_id, stop_id, empty_id, full_id, mutex, rear_id;
+
+	fscanf(fp, "%d", &buffer_size);
+	fscanf(fp, "%d", &buffer_id);
+	fscanf(fp, "%d", &stop_id);
+	fscanf(fp, "%d", &empty_id);
+	fscanf(fp, "%d", &full_id);
+	fscanf(fp, "%d", &mutex);
+	fscanf(fp, "%d", &rear_id);
+	fclose(fp);
+
+	// Attach to shared memory
+	struct Job* buffer_addr = (struct Job*) shmat(buffer_id, NULL, SHM_RND);
+	int* rear_addr = (int*) shmat(rear_id, NULL, SHM_RND);
+	int* stop = (int*) shmat(stop_id, NULL, SHM_RND);
+
+	int i, sleep_duration;
+	srand(pid);
+	// Repeatedly submit print jobs
+	for (i = 0; i < 5; i++) {
+		sleep_duration = (rand() % 4) + 2;
+		printf("User %d is working for %d seconds\n", pid, sleep_duration);
+		sleep(sleep_duration);
+		if (*stop) break;
+		printf("User %d is printing %s\n", pid, print_job.filename);
+		p(0, empty_id);
+		p(0, mutex);
+		buffer_addr[*rear_addr].owner_id = print_job.owner_id;
+		strcpy(buffer_addr[*rear_addr].filename, print_job.filename);
+		*rear_addr = (*rear_addr + 1) % buffer_size;
+		v(0, mutex);
+		v(0, full_id);
+	}
+
+	printf("User %d is logging off\n", pid);
+
+	return 0;
+}
+
+p(int s,int sem_id) {
+        struct sembuf sops;
+
+        sops.sem_num = s;
+        sops.sem_op = -1;
+        sops.sem_flg = 0;
+        if((semop(sem_id, &sops, 1)) == -1) printf("%s", "'P' error\n");
+}
+
+v(int s, int sem_id) {
+        struct sembuf sops;
+
+        sops.sem_num = s;
+        sops.sem_op = 1;
+        sops.sem_flg = 0;
+        if((semop(sem_id, &sops, 1)) == -1) printf("%s","'V' error\n");
+}
